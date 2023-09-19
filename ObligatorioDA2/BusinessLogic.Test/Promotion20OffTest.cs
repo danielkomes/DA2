34aa87d128fbbe9e534b34d797e900057ea155a7
Daniel Komés <danielkomes@gmail.com>
2023-09-19 18:39:36 -0300

    Add Test1Product

--- ObligatorioDA2/BusinessLogic.Test/Promotion20OffTest.cs
+++ ObligatorioDA2/BusinessLogic.Test/Promotion20OffTest.cs
@@ -15,6 +15,20 @@ namespace BusinessLogic.Test
             Assert.AreEqual(0, total);
         }
 
+        [TestMethod]
+        public void Test1Product()
+        {
+            IPromotion p = new Promotion20Off();
+            List<Product> products = new List<Product>();
+            Product p1 = new Product()
+            {
+                Price = 100
+            };
+            products.Add(p1);
+            float total = p.GetTotal(products);
+            Assert.AreEqual(100, total);
+        }
+
 
 
     }
