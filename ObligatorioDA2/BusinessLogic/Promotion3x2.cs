38d9209e082e8dd9151b2ba6ae322ab0688a0771
Daniel Komés <danielkomes@gmail.com>
2023-09-19 20:40:01 -0300

    Update GetTotal, tests passing

--- ObligatorioDA2/BusinessLogic/Promotion3x2.cs
+++ ObligatorioDA2/BusinessLogic/Promotion3x2.cs
@@ -7,10 +7,20 @@ namespace BusinessLogic
     {
         public float GetTotal(IEnumerable<Product> products)
         {
+            if (products.Count() == 0) return 0;
             float total = 0;
+            float minPrice = products.ElementAt(0).Price;
             foreach (Product product in products)
             {
                 total += product.Price;
+                if (product.Price < minPrice)
+                {
+                    minPrice = product.Price;
+                }
+            }
+            if (products.Count() >= 3)
+            {
+                total -= minPrice;
             }
             return total;
         }
