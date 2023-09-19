823b37731579ba6cbe98403e5d262cc742da898f
Daniel Komés <danielkomes@gmail.com>
2023-09-19 18:32:18 -0300

    Add TestNoProducts

--- ObligatorioDA2/BusinessLogic.Test/Promotion20OffTest.cs
+++ ObligatorioDA2/BusinessLogic.Test/Promotion20OffTest.cs
@@ -6,7 +6,14 @@ namespace BusinessLogic.Test
     [TestClass]
     public class Promotion20OffTest
     {
-
+        [TestMethod]
+        public void TestNoProducts()
+        {
+            IPromotion p = new Promotion20Off();
+            IEnumerable<Product> products = new List<Product>();
+            float total = p.GetTotal(products);
+            Assert.AreEqual(0, total);
+        }
 
 
 
