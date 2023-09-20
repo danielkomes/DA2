558625659828c5101cf196057975febbe1ffe7be
Daniel Komés <danielkomes@gmail.com>
2023-09-19 23:16:59 -0300

    Add Test2Products, tests passing

--- ObligatorioDA2/BusinessLogic.Test/PromotionTotalLookTest.cs
+++ ObligatorioDA2/BusinessLogic.Test/PromotionTotalLookTest.cs
@@ -30,6 +30,28 @@ namespace BusinessLogic.Test
             Assert.AreEqual(100, total);
         }
 
+        [TestMethod]
+        public void Test2Products()
+        {
+            IPromotion p = new PromotionTotalLook();
+            List<Product> products = new List<Product>();
+            Product p1 = new Product()
+            {
+                Price = 100,
+                Colors = new List<string> { "red" }
+            };
+            Product p2 = new Product()
+            {
+                Price = 200,
+                Colors = new List<string> { "red" }
+            };
+            products.Add(p1);
+            products.Add(p2);
+            float total = p.GetTotal(products);
+            float expected = 100 + 200;
+            Assert.AreEqual(expected, total);
+        }
+
 
     }
 }
