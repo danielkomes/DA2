1bb03ca9fb8977abd77f3454a9bbec64a029aac3
Daniel Komés <danielkomes@gmail.com>
2023-09-20 00:58:18 -0300

    Add shopping cart interface and implementing class

---
+++ ObligatorioDA2/BusinessLogic/ShoppingCart.cs
@@ -0,0 +1,36 @@
+﻿using Domain;
+using IBusinessLogic;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLogic
+{
+    public class ShoppingCart : IShoppingCart
+    {
+        public User User { get; set; }
+        public IEnumerable<Product> ProductsChecked { get; set; }
+
+        public void AddToCart(Product product)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DoPurchase()
+        {
+            throw new NotImplementedException();
+        }
+
+        public float GetTotalPrice()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RemoveFromCart(Product product)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ ObligatorioDA2/IBusinessLogic/IShoppingCart.cs
@@ -0,0 +1,16 @@
+﻿using Domain;
+
+namespace IBusinessLogic
+{
+    public interface IShoppingCart
+    {
+        public User User { get; set; }
+        public IEnumerable<Product> ProductsChecked { get; set; }
+
+
+        public void AddToCart(Product product);
+        public void RemoveFromCart(Product product);
+        public float GetTotalPrice();
+        public void DoPurchase();
+    } 
+}
