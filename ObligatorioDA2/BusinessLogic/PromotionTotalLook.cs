f91c3e232cd9505a3d56e0dd38aa1da4d385fdd9
Daniel Komés <danielkomes@gmail.com>
2023-09-19 23:29:59 -0300

    Update GetTotal, tests passing

--- ObligatorioDA2/BusinessLogic/PromotionTotalLook.cs
+++ ObligatorioDA2/BusinessLogic/PromotionTotalLook.cs
@@ -7,10 +7,32 @@ namespace BusinessLogic
     {
         public float GetTotal(IEnumerable<Product> products)
         {
+            if (products.Count() == 0) return 0;
             float total = 0;
+            float maxPrice = products.ElementAt(0).Price;
+            Dictionary<string, int> colorCount = new Dictionary<string, int>();
             foreach (Product product in products)
             {
                 total += product.Price;
+                foreach (string color in product.Colors)
+                {
+                    if (colorCount.ContainsKey(color))
+                    {
+                        colorCount[color]++;
+                    }
+                    else
+                    {
+                        colorCount.Add(color, 1);
+                    }
+                }
+                if (product.Price > maxPrice)
+                {
+                    maxPrice = product.Price;
+                }
+            }
+            if (colorCount.Values.Max() >= 3)
+            {
+                total -= maxPrice * 0.5f;
             }
             return total;
         }
