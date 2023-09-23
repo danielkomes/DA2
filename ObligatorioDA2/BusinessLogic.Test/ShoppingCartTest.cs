d6e0b57e065c642b75c144d27e2f22d2e27a014d
Daniel Komés <danielkomes@gmail.com>
2023-09-22 21:25:33 -0300

    Add test AddToCartInvalid

--- ObligatorioDA2/BusinessLogic.Test/ShoppingCartTest.cs
+++ ObligatorioDA2/BusinessLogic.Test/ShoppingCartTest.cs
@@ -36,6 +36,23 @@ namespace BusinessLogic.Test
             helperMock.VerifyAll();
         }
 
+        [TestMethod]
+        public void AddToCartInvalid()
+        {
+            Product p = new Product();
+            var userMock = new Mock<IService<User>>(MockBehavior.Strict);
+            var productMock = new Mock<IService<Product>>(MockBehavior.Strict);
+            var purchaseMock = new Mock<IService<PromotionEntity>>(MockBehavior.Strict);
+            var helperMock = new Mock<IShoppingCartDataAccessHelper>(MockBehavior.Strict);
+            IShoppingCartDataAccessHelper helper = new ShoppingCartDataAccessHelper(
+                userMock.Object, productMock.Object, purchaseMock.Object);
+            helperMock.Setup(sp => sp.VerifyProduct(p)).Returns(false);
+
+            ShoppingCart cart = new ShoppingCart(helperMock.Object);
+            cart.AddToCart(p);
+
+            Assert.IsTrue(!cart.ProductsChecked.Any());
+            helperMock.VerifyAll();
         }
     }
 }
