5a82daa58158f2b287c62079cd1102e6c1b07abf
Daniel Komés <danielkomes@gmail.com>
2023-09-23 20:08:04 -0300

    Add GetTotalPrice2Product2Promotion test

--- ObligatorioDA2/BusinessLogic.Test/ShoppingCartTest.cs
+++ ObligatorioDA2/BusinessLogic.Test/ShoppingCartTest.cs
@@ -134,5 +134,44 @@ namespace BusinessLogic.Test
             Assert.AreEqual(expected, actual);
             helperMock.VerifyAll();
         }
+
+        [TestMethod]
+        public void GetTotalPrice2Product2Promotion()
+        {
+            Product p1 = new Product()
+            {
+                Price = 100,
+                Category = "cat1"
+            };
+            Product p2 = new Product()
+            {
+                Price = 200,
+                Category = "cat1"
+            };
+            PromotionEntity promotionEntity = new PromotionEntity();
+            Promotion20Off promotion20Off = new Promotion20Off(promotionEntity);
+            Promotion3x2 promotion3x2 = new Promotion3x2(promotionEntity);
+            List<PromotionAbstract> promotionsList = new List<PromotionAbstract> {
+                promotion20Off, promotion3x2
+            };
+
+            var userMock = new Mock<IService<User>>(MockBehavior.Strict);
+            var productMock = new Mock<IService<Product>>(MockBehavior.Strict);
+            var promotionMock = new Mock<IService<PromotionEntity>>(MockBehavior.Strict);
+            var helperMock = new Mock<IShoppingCartDataAccessHelper>(MockBehavior.Strict);
+            IShoppingCartDataAccessHelper helper = new ShoppingCartDataAccessHelper(
+                userMock.Object, productMock.Object, promotionMock.Object);
+            helperMock.Setup(sp => sp.GetPromotions())
+                .Returns(promotionsList);
+            helperMock.Setup(sp => sp.VerifyProduct(It.IsAny<Product>())).Returns(true);
+
+            ShoppingCart cart = new ShoppingCart(helperMock.Object);
+            cart.AddToCart(p1);
+            cart.AddToCart(p2);
+            float actual = cart.GetTotalPrice();
+            float expected = 100 + 200 - 200 * 0.2f;
+            Assert.AreEqual(expected, actual);
+            helperMock.VerifyAll();
+        }
     }
 }
