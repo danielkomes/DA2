08429835a8ebab9c6559c09f74a8c55be73e0b54
Daniel Komés <danielkomes@gmail.com>
2023-09-24 00:49:53 -0300

    Add test VerifyUserValid

--- ObligatorioDA2/BusinessLogic.Test/ShoppingCartDataAccessHelperTest.cs
+++ ObligatorioDA2/BusinessLogic.Test/ShoppingCartDataAccessHelperTest.cs
@@ -44,5 +44,21 @@ namespace BusinessLogic.Test
             bool actual = helper.VerifyProduct(p);
             Assert.IsFalse(actual);
         }
+
+        [TestMethod]
+        public void VerifyUserValid()
+        {
+            User u = new User();
+            var userMock = new Mock<IService<User>>(MockBehavior.Strict);
+            var productMock = new Mock<IService<Product>>(MockBehavior.Strict);
+            var promotionMock = new Mock<IService<PromotionEntity>>(MockBehavior.Strict);
+            var purchaseMock = new Mock<IService<Purchase>>(MockBehavior.Strict);
+            var helperMock = new Mock<IShoppingCartDataAccessHelper>(MockBehavior.Strict);
+            IShoppingCartDataAccessHelper helper = new ShoppingCartDataAccessHelper(
+                userMock.Object, productMock.Object, promotionMock.Object, purchaseMock.Object);
+            userMock.Setup(m => m.Exists(u.Id)).Returns(true);
+            bool actual = helper.VerifyUser(u);
+            Assert.IsTrue(actual);
+        }
     }
 }
