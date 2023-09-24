b9af64c045722061f40e6f110f8d2dba21ac7909
Daniel Komés <danielkomes@gmail.com>
2023-09-24 00:44:38 -0300

    Add test VerifyProductValid

--- ObligatorioDA2/BusinessLogic.Test/ShoppingCartDataAccessHelperTest.cs
+++ ObligatorioDA2/BusinessLogic.Test/ShoppingCartDataAccessHelperTest.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using Domain;
+using IBusinessLogic;
+using IDataAccess;
+using Moq;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,5 +13,20 @@ namespace BusinessLogic.Test
     [TestClass]
     public class ShoppingCartDataAccessHelperTest
     {
+        [TestMethod]
+        public void VerifyProductValid()
+        {
+            Product p = new Product();
+            var userMock = new Mock<IService<User>>(MockBehavior.Strict);
+            var productMock = new Mock<IService<Product>>(MockBehavior.Strict);
+            var promotionMock = new Mock<IService<PromotionEntity>>(MockBehavior.Strict);
+            var purchaseMock = new Mock<IService<Purchase>>(MockBehavior.Strict);
+            var helperMock = new Mock<IShoppingCartDataAccessHelper>(MockBehavior.Strict);
+            IShoppingCartDataAccessHelper helper = new ShoppingCartDataAccessHelper(
+                userMock.Object, productMock.Object, promotionMock.Object, purchaseMock.Object);
+            productMock.Setup(m => m.Exists(p.Id)).Returns(true);
+            bool actual = helper.VerifyProduct(p);
+            Assert.IsTrue(actual);
+        }
     }
 }
