39051f50e1ee63fe86174e5d7b71fe454acd5a96
Daniel Komés <danielkomes@gmail.com>
2023-09-26 03:19:21 -0300

    Fix UserControllerTest to add VerifyAll and use IEnumerable instead of List

--- ObligatorioDA2/WebApi.Test/UserControllerTest.cs
+++ ObligatorioDA2/WebApi.Test/UserControllerTest.cs
@@ -14,8 +14,8 @@ namespace WebApi.Test
         public void GetAll1User()
         {
             User user1 = new User();
-            List<User> users = new List<User> { user1 };
-            List<UserModelOut> userModelOuts = new List<UserModelOut> { new UserModelOut(user1) };
+            IEnumerable<User> users = new List<User> { user1 };
+            IEnumerable<UserModelOut> userModelOuts = new List<UserModelOut> { new UserModelOut(user1) };
             var userMock = new Mock<IService<User>>(MockBehavior.Strict);
             UserController userController = new UserController(userMock.Object);
             userMock.Setup(m => m.GetAll()).Returns(users);
@@ -26,14 +26,15 @@ namespace WebApi.Test
             Assert.AreEqual(expected.GetType(), actual.GetType());
 
             OkObjectResult actualOk = actual as OkObjectResult;
-            List<UserModelOut> actualModels = actualOk.Value as List<UserModelOut>;
+            IEnumerable<UserModelOut> actualModels = actualOk.Value as IEnumerable<UserModelOut>;
             OkObjectResult expectedOk = expected as OkObjectResult;
-            List<UserModelOut> expectedModels = expectedOk.Value as List<UserModelOut>;
+            IEnumerable<UserModelOut> expectedModels = expectedOk.Value as IEnumerable<UserModelOut>;
             for (int i = 0; i < userModelOuts.Count(); i++)
             {
-                Assert.AreEqual(expectedModels[i].Email, actualModels[i].Email);
-                Assert.AreEqual(expectedModels[i].Address, actualModels[i].Address);
+                Assert.AreEqual(expectedModels.ElementAt(i).Email, actualModels.ElementAt(i).Email);
+                Assert.AreEqual(expectedModels.ElementAt(i).Address, actualModels.ElementAt(i).Address);
             }
+            userMock.VerifyAll();
         }
 
         [TestMethod]
@@ -42,13 +43,13 @@ namespace WebApi.Test
             User user1 = new User();
             User user2 = new User();
             User user3 = new User();
-            List<User> users = new List<User>
+            IEnumerable<User> users = new List<User>
             {
                 user1,
                 user2,
                 user3
             };
-            List<UserModelOut> userModelOuts = new List<UserModelOut>
+            IEnumerable<UserModelOut> userModelOuts = new List<UserModelOut>
             {
                 new UserModelOut(user1),
                 new UserModelOut(user2),
@@ -65,14 +66,15 @@ namespace WebApi.Test
             Assert.AreEqual(expected.GetType(), actual.GetType());
 
             OkObjectResult actualOk = actual as OkObjectResult;
-            List<UserModelOut> actualModels = actualOk.Value as List<UserModelOut>;
+            IEnumerable<UserModelOut> actualModels = actualOk.Value as IEnumerable<UserModelOut>;
             OkObjectResult expectedOk = expected as OkObjectResult;
-            List<UserModelOut> expectedModels = expectedOk.Value as List<UserModelOut>;
+            IEnumerable<UserModelOut> expectedModels = expectedOk.Value as IEnumerable<UserModelOut>;
             for (int i = 0; i < userModelOuts.Count(); i++)
             {
-                Assert.AreEqual(expectedModels[i].Email, actualModels[i].Email);
-                Assert.AreEqual(expectedModels[i].Address, actualModels[i].Address);
+                Assert.AreEqual(expectedModels.ElementAt(i).Email, actualModels.ElementAt(i).Email);
+                Assert.AreEqual(expectedModels.ElementAt(i).Address, actualModels.ElementAt(i).Address);
             }
+            userMock.VerifyAll();
         }
     }
 }
