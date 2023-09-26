f8d04d3ab8b1d0079af7ee6ec80ab7006c3e81e5
Daniel Komés <danielkomes@gmail.com>
2023-09-26 03:14:22 -0300

    Add test GetAll3Users

--- ObligatorioDA2/WebApi.Test/UserControllerTest.cs
+++ ObligatorioDA2/WebApi.Test/UserControllerTest.cs
@@ -35,5 +35,44 @@ namespace WebApi.Test
                 Assert.AreEqual(expectedModels[i].Address, actualModels[i].Address);
             }
         }
+
+        [TestMethod]
+        public void GetAll3Users()
+        {
+            User user1 = new User();
+            User user2 = new User();
+            User user3 = new User();
+            List<User> users = new List<User>
+            {
+                user1,
+                user2,
+                user3
+            };
+            List<UserModelOut> userModelOuts = new List<UserModelOut>
+            {
+                new UserModelOut(user1),
+                new UserModelOut(user2),
+                new UserModelOut(user3)
+            };
+
+            var userMock = new Mock<IService<User>>(MockBehavior.Strict);
+            UserController userController = new UserController(userMock.Object);
+            userMock.Setup(m => m.GetAll()).Returns(users);
+
+
+            IActionResult actual = userController.GetAll();
+            IActionResult expected = new OkObjectResult(userModelOuts);
+            Assert.AreEqual(expected.GetType(), actual.GetType());
+
+            OkObjectResult actualOk = actual as OkObjectResult;
+            List<UserModelOut> actualModels = actualOk.Value as List<UserModelOut>;
+            OkObjectResult expectedOk = expected as OkObjectResult;
+            List<UserModelOut> expectedModels = expectedOk.Value as List<UserModelOut>;
+            for (int i = 0; i < userModelOuts.Count(); i++)
+            {
+                Assert.AreEqual(expectedModels[i].Email, actualModels[i].Email);
+                Assert.AreEqual(expectedModels[i].Address, actualModels[i].Address);
+            }
+        }
     }
 }
