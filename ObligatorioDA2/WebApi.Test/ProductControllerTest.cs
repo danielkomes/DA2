681cf8a84a08786cf9cba78862837256b235b2c3
Daniel Komés <danielkomes@gmail.com>
2023-09-26 21:38:45 -0300

    Add test GetProductsByPartialNameOk, tests passing

--- ObligatorioDA2/WebApi.Test/ProductControllerTest.cs
+++ ObligatorioDA2/WebApi.Test/ProductControllerTest.cs
@@ -120,5 +120,47 @@ namespace WebApi.Test
             }
             productMock.VerifyAll();
         }
+
+        [TestMethod]
+        public void GetProductsByPartialNameOk()
+        {
+            Product p1 = new Product()
+            {
+                Name = "p1"
+            };
+            Product p2 = new Product()
+            {
+                Name = "p1"
+            };
+            Product p3 = new Product()
+            {
+                Name = "p2"
+            };
+            IEnumerable<Product> products = new List<Product> { p1, p2, p3 };
+            IEnumerable<ProductModelOut> productModels = new List<ProductModelOut>
+            {
+                new ProductModelOut(p1),
+                new ProductModelOut(p2),
+            };
+            var productMock = new Mock<IService<Product>>(MockBehavior.Strict);
+            ProductController productController = new ProductController(productMock.Object);
+            productMock.Setup(m => m.FindByCondition(It.IsAny<Expression<Func<Product, bool>>>())).Returns(products);
+
+
+            IActionResult actual = productController.GetAll("1");
+            IActionResult expected = new OkObjectResult(productModels);
+
+            Assert.AreEqual(expected.GetType(), actual.GetType());
+            OkObjectResult actualOk = actual as OkObjectResult;
+            IEnumerable<ProductModelOut> actualModels = actualOk.Value as IEnumerable<ProductModelOut>;
+            OkObjectResult expectedOk = expected as OkObjectResult;
+            IEnumerable<ProductModelOut> expectedModels = expectedOk.Value as IEnumerable<ProductModelOut>;
+            for (int i = 0; i < productModels.Count(); i++)
+            {
+                Assert.AreEqual(expectedModels.ElementAt(i).Name, actualModels.ElementAt(i).Name);
+                Assert.AreEqual(expectedModels.ElementAt(i).Description, actualModels.ElementAt(i).Description);
+            }
+            productMock.VerifyAll();
+        }
     }
 }
