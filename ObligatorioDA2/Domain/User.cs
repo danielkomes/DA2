3de5aedf968b93383527bb12b9b9e7f1fd4aa878
Daniel Komés <danielkomes@gmail.com>
2023-09-27 22:19:32 -0300

    Update User to have a list of roles. Add Authentication and Authorization filters

--- ObligatorioDA2/Domain/User.cs
+++ ObligatorioDA2/Domain/User.cs
@@ -4,12 +4,12 @@
     {
         public Guid Id { get; set; }
         public string Email { get; set; }
-        public bool IsAdmin { get; set; }
         public string Address { get; set; }
-
+        public IEnumerable<EUserRole> Roles { get; set; }
         public User()
         {
             Id = Guid.NewGuid();
+            Roles = new List<EUserRole>();
         }
     }
 }
---
+++ ObligatorioDA2/WebApi/Filters/AuthenticationFilter.cs
@@ -0,0 +1,42 @@
+﻿using Domain;
+using IBusinessLogic;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using System.Diagnostics.Eventing.Reader;
+
+namespace WebApi.Filters
+{
+    public class AuthenticationFilter : Attribute, IAuthorizationFilter
+    {
+        private readonly ISessionLogic SessionLogic;
+        public AuthenticationFilter(ISessionLogic sessionLogic)
+        {
+            SessionLogic = sessionLogic;
+        }
+
+        public void OnAuthorization(AuthorizationFilterContext context)
+        {
+            var header = context.HttpContext.Request.Headers["Authorization"].ToString();
+            Guid token = Guid.Empty;
+
+            if (string.IsNullOrEmpty(header) || !Guid.TryParse(header, out token))
+            {
+                context.Result = new ObjectResult(new { Message = "Authorization header missing" })
+                {
+                    StatusCode = StatusCodes.Status401Unauthorized
+                };
+            }
+            else
+            {
+                User currentUser = SessionLogic.GetCurrentUser(token);
+                if (currentUser is null)
+                {
+                    context.Result = new ObjectResult(new { Message = "Unauthorized" })
+                    {
+                        StatusCode = StatusCodes.Status401Unauthorized
+                    };
+                }
+            }
+        }
+    }
+}
