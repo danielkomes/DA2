112b79f1c23f4a3c10220a9487f14f86313c3e64
Daniel Komés <danielkomes@gmail.com>
2023-10-05 04:47:26 -0300

    Add test AddValid

--- ObligatorioDA2/BusinessLogic.Test/UserLogicTest.cs
+++ ObligatorioDA2/BusinessLogic.Test/UserLogicTest.cs
@@ -9,5 +9,23 @@ namespace BusinessLogic.Test
     public class UserLogicTest
     {
 
+        [TestMethod]
+        public void AddValid()
+        {
+            User user = new User()
+            {
+                Email = "user@test.com"
+            };
+            var userMock = new Mock<IService<User>>(MockBehavior.Strict);
+            var sessionMock = new Mock<ISessionLogic>(MockBehavior.Strict);
+            UserLogic logic = new UserLogic(userMock.Object, sessionMock.Object);
+            userMock.Setup(m => m.Add(user));
+            sessionMock.Setup(m => m.GetCurrentUser(null)).Returns(user);
+
+            logic.Add(user);
+
+            userMock.VerifyAll();
+            sessionMock.VerifyAll();
+        }
     }
 }
