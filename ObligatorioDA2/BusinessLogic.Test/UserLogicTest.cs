516e696a0a6c42a1d69762f4d43f7f6a6136688f
Daniel Komés <danielkomes@gmail.com>
2023-10-10 15:30:37 -0300

    Add test GetAllValid

--- ObligatorioDA2/BusinessLogic.Test/UserLogicTest.cs
+++ ObligatorioDA2/BusinessLogic.Test/UserLogicTest.cs
@@ -59,7 +59,35 @@ namespace BusinessLogic.Test
 
             User actual = logic.Get(user.Email);
 
-            Assert.AreEqual(user,actual);
+            Assert.AreEqual(user, actual);
+            userMock.VerifyAll();
+        }
+
+        [TestMethod]
+        public void GetAllValid()
+        {
+            User user1 = new User()
+            {
+                Email = "user@test.com"
+            };
+            User user2 = new User()
+            {
+                Email = "user@test.com"
+            };
+            IEnumerable<User> users = new List<User>() { user1, user2 };
+            var userMock = new Mock<IService<User>>(MockBehavior.Strict);
+            var sessionMock = new Mock<ISessionLogic>(MockBehavior.Strict);
+            UserLogic logic = new UserLogic(userMock.Object, sessionMock.Object);
+            userMock.Setup(m => m.GetAll()).Returns(users);
+
+            IEnumerable<User> actual = logic.GetAll();
+
+            for (int i = 0; i < users.Count(); i++)
+            {
+                Assert.AreEqual(users.ElementAt(i).Id, actual.ElementAt(i).Id);
+                Assert.AreEqual(users.ElementAt(i).Email, actual.ElementAt(i).Email);
+                Assert.AreEqual(users.ElementAt(i).Address, actual.ElementAt(i).Address);
+            }
             userMock.VerifyAll();
         }
     }
