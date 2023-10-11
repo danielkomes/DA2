1c0cb932d9ed6ba9f52672e3d8f384b6dd93bd8f
Daniel Komés <danielkomes@gmail.com>
2023-10-11 12:43:10 -0300

    Add SignupLogic interface, class and test class. Add test SignupOk

---
+++ ObligatorioDA2/BusinessLogic.Test/SignupLogicTest.cs
@@ -0,0 +1,38 @@
+﻿using Domain;
+using IDataAccess;
+using Moq;
+
+namespace BusinessLogic.Test
+{
+    [TestClass]
+    public class SignupLogicTest
+    {
+        private Mock<IService<User>> UserMock { get; set; }
+        private SignupLogic SignupLogic { get; set; }
+
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            UserMock = new Mock<IService<User>>(MockBehavior.Strict);
+            SignupLogic = new SignupLogic(UserMock.Object);
+        }
+
+        [TestCleanup]
+        public void TestCleanup()
+        {
+            UserMock.VerifyAll();
+        }
+
+        [TestMethod]
+        public void SignupOk()
+        {
+            User user = new User()
+            {
+                Email = "user@test.com"
+            };
+            UserMock.Setup(m => m.Add(user));
+
+            SignupLogic.Signup(user);
+        }
+    }
+}
---
+++ ObligatorioDA2/BusinessLogic/SignupLogic.cs
@@ -0,0 +1,21 @@
+﻿using Domain;
+using IBusinessLogic;
+using IDataAccess;
+
+namespace BusinessLogic
+{
+    public class SignupLogic : ISignupLogic
+    {
+        private readonly IService<User> UserService;
+
+        public SignupLogic(IService<User> userService)
+        {
+            UserService = userService;
+        }
+
+        public void Signup(User user)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
