ef9a6b5721d09751d60f485f52b5ec219acab871
Daniel Komés <danielkomes@gmail.com>
2023-10-11 00:21:06 -0300

    Add test GetAny

--- ObligatorioDA2/BusinessLogic.Test/ProductLogicTest.cs
+++ ObligatorioDA2/BusinessLogic.Test/ProductLogicTest.cs
@@ -8,6 +8,7 @@ using System.Linq;
 using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 
 namespace BusinessLogic.Test
 {
@@ -74,6 +75,33 @@ namespace BusinessLogic.Test
             ProductMock.Setup(m => m.FindByCondition(It.IsAny<Expression<Func<Product, bool>>>())).Returns(products);
 
             IEnumerable<Product> actual = ProductLogic.FindByCondition("1", null, null);
+            IEnumerable<Product> expected = products;
+
+            for (int i = 0; i < expected.Count(); i++)
+            {
+                Assert.AreEqual(expected.ElementAt(i).Name, actual.ElementAt(i).Name);
+            }
+        }
+
+        [TestMethod]
+        public void FindAny()
+        {
+            Product p1 = new Product()
+            {
+                Name = "p1",
+            };
+            Product p2 = new Product()
+            {
+                Name = "p2",
+            };
+            Product p3 = new Product()
+            {
+                Name = "p3",
+            };
+            IEnumerable<Product> products = new List<Product>() { p1, p2, p3 };
+            ProductMock.Setup(m => m.GetAll()).Returns(products);
+
+            IEnumerable<Product> actual = ProductLogic.FindByCondition(null, null, null);
             IEnumerable<Product> expected = products;
 
             for (int i = 0; i < expected.Count(); i++)
