c5c8de14543d96467deebb2acfc6416655d0a0ac
Daniel Komés <danielkomes@gmail.com>
2023-10-13 22:27:53 -0300

    Update User models to include password

--- ObligatorioDA2/WebApi/Models/In/UserModelInForCustomers.cs
+++ ObligatorioDA2/WebApi/Models/In/UserModelInForCustomers.cs
@@ -5,6 +5,7 @@ namespace WebApi.Models.In
     public class UserModelInForCustomers
     {
         public string Email { get; set; }
+        public string Password { get; set; }
         public string Address { get; set; }
 
         public User ToEntity()
@@ -12,6 +13,7 @@ namespace WebApi.Models.In
             return new User()
             {
                 Email = Email,
+                Password = Password,
                 Address = Address
             };
         }
--- ObligatorioDA2/WebApi/Models/Out/UserModelOutForAdmins.cs
+++ ObligatorioDA2/WebApi/Models/Out/UserModelOutForAdmins.cs
@@ -5,12 +5,14 @@ namespace WebApi.Models.Out
     public class UserModelOutForAdmins
     {
         public string Email { get; set; }
+        public string Password { get; set; }
         public string Address { get; set; }
         public IEnumerable<EUserRole> Roles { get; set; }
 
         public UserModelOutForAdmins(User user)
         {
             Email = user.Email;
+            Password = user.Password;
             Address = user.Address;
             Roles = user.Roles;
         }
