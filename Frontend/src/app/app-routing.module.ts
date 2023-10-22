ee187c697447f7de796240efdff0a722f7a125ba
Daniel Komés <danielkomes@gmail.com>
2023-10-22 18:25:21 -0300

    Add register-page and content

--- Frontend/src/app/app-routing.module.ts
+++ Frontend/src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginPageComponent } from './login-page/login-page.component';
+import { RegisterPageComponent } from './register-page/register-page.component';
 
 const routes: Routes = [
   { path: 'login', component: LoginPageComponent, pathMatch: 'full' },
+  { path: 'register', component: RegisterPageComponent, pathMatch: 'full' },
 ];
 
 @NgModule({
--- Frontend/src/app/app.module.ts
+++ Frontend/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { RegisterPageComponent } from './register-page/register-page.component';
 // import { LoginPageComponent } from './login-page/login-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    RegisterPageComponent,
     // LoginPageComponent
   ],
   imports: [BrowserModule, AppRoutingModule, NgbModule],
