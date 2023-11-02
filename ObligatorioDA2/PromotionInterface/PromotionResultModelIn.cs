7a62d4d4e8b81e51afffa19b85f0c17cfdd305e0
Daniel Komés <danielkomes@gmail.com>
2023-11-02 00:16:03 -0300

    Add PromotionInterface project to hold the classes needed for third parties to develop new Promotions. Add PromotionResultModelIn and ProductModelIn to represent their counterparts in Domain.

---
+++ ObligatorioDA2/PromotionInterface/PromotionResultModelIn.cs
@@ -0,0 +1,14 @@
+﻿namespace PromotionInterface
+{
+    public class PromotionResultModelIn
+    {
+        public readonly float Result;
+        public readonly bool IsApplied;
+
+        public PromotionResultModelIn(float result, bool isApplied)
+        {
+            Result = result;
+            IsApplied = isApplied;
+        }
+    }
+}
